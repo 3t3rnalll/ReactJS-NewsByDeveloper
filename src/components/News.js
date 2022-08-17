339e5086ac5fe1a9e771efe75849cdf7d9c6a805
3t3rnalll <shubhamrajvarun@gmail.com>
2022-08-17 13:52:42 +0530

    add Cards to show  and generate news api

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,23 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                News Components
-                <NewsItem />
-                <NewsItem />
+            <div className='container my-4'>
+                <h2>News -- Top Headlines</h2>
+                <hr className='my-4' />
+                <div className="row">
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' URL='https://www.google.com' urlToImage="https://static01.nyt.com/images/2022/08/04/sports/04cricket-climate-1/04cricket-climate-1-facebookJumbo.jpg" />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' URL='https://www.google.com' urlToImage="https://static01.nyt.com/images/2022/08/04/sports/04cricket-climate-1/04cricket-climate-1-facebookJumbo.jpg" />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' URL='https://www.google.com' urlToImage="https://static01.nyt.com/images/2022/08/04/sports/04cricket-climate-1/04cricket-climate-1-facebookJumbo.jpg" />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' URL='https://www.google.com' urlToImage="https://static01.nyt.com/images/2022/08/04/sports/04cricket-climate-1/04cricket-climate-1-facebookJumbo.jpg" />
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let { title, description, url, urlToImage } = this.props;
         return (
             <div>
-                This is News Items Components
+                <div className="card my-3 mx-3" style={{ width: '18rem' }}>
+                    <img src={urlToImage} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={url} className="card-link">Learn More</a>
+                    </div>
+                </div>
             </div>
         )
     }
